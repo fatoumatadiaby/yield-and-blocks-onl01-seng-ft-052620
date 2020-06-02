d616434b8b451f2f965dce3f37d8e5998d814ac1
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-02 14:15:27 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,16 +2,16 @@ def hello_t(array)
     ["Tim", "Tom", "Jim"].each do |names|
   puts "#{names}"
   end
-end
+# end
   
-def hello_t(array) 
-  i = 0
+# def hello_t(array) 
+#   i = 0
   
-  while i < array.length 
-  i = i + 1 
-  yield array[1]
-end 
+#   while i < array.length 
+#   i = i + 1 
+#   yield array[1]
+# end 
 
-    array 
-end
+#     array 
+# end
  
